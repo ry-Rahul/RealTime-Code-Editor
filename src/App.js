175c4c2b9a391f9fee3740039c05350309b19e98
Rahul Yadav <ryrahul12345@gmail.com>
2023-09-18 16:55:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
 import { Toaster } from "react-hot-toast";
+import React from 'react';
 
-function App() {
+ function App() {
     return (
-        <>
+    <>
             <div>
                 <Toaster
                     position="top-right"
