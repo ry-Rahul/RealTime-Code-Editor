2b13e2670fa7ff3fe92c4006183bd32a357d60d5
Rahul Yadav <ryrahul12345@gmail.com>
2023-09-18 16:32:59 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
 import { Toaster } from "react-hot-toast";
+import React from 'react';
 
 function App() {
     return (
